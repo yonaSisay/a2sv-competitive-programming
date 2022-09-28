e2933169f123ccc1fbf5281b70552f7d30e6546c
yonas sisay <yonimelkamu357@gmail.com>
2022-09-28 11:26:08 +0300

    domino piling

---
+++ dominopiling.java
@@ -0,0 +1,10 @@
+
+public class program{
+    public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int k=n*m/2;
+        System.out.println(k);
+    }
+}
