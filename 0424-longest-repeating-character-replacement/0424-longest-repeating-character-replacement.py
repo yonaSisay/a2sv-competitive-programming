a017c217c72b0e359f6770a565817cb6cc80d01e
yonas sisay <86576073+yonaSisay@users.noreply.github.com>
2023-02-23 08:05:29 -0800

    Time: 301 ms (18.71%), Space: 13.9 MB (51.27%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,14 +3,10 @@ class Solution:
         seen = Counter()
         l = 0
         maxx = 0
-        
         for i in range(len(s)):
             seen[s[i]] += 1
             while seen.total() > max(seen.values()) + k:
                 seen[s[l]] -= 1
-                if seen[s[l]] == 0:
-                    del seen[s[l]]
                 l += 1
             maxx = max(seen.total(), maxx)
-        
         return maxx
