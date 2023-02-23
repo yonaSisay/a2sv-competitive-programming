bcd058eed139d939725ceb34da629829548d7117
yonas sisay <86576073+yonaSisay@users.noreply.github.com>
2023-02-23 03:51:14 -0800

    Time: 1101 ms (19.24%), Space: 20.5 MB (29.06%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,20 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         mapp = defaultdict(int)
-        maxx = left = right = 0
+        maxx = 0
+        left = 0
         
-        while right < len(fruits) and left < len(fruits):
-            if len(mapp) != 3:
-                mapp[fruits[right]] += 1
-                right += 1
-            if len(mapp) == 3:
+        for i in range(len(fruits)):
+            mapp[fruits[i]] += 1
+            while  len(mapp) > 2:
                 mapp[fruits[left]] -= 1
                 if mapp[fruits[left]] == 0:
                     del mapp[fruits[left]]
-                left += 1
-            if len(mapp) <= 2:
-                temp = 0
-                for num in mapp:
-                    temp += mapp[num] 
-                maxx = max(temp,maxx)
-        return maxx            
+                left += 1  
+            maxx = max(sum(mapp.values()) , maxx)
+        return maxx
+               
