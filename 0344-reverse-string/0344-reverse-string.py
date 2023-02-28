03e8581bbe68ad3ddfd60d8d37d45a92c7dbd280
yonas sisay <86576073+yonaSisay@users.noreply.github.com>
2023-02-28 11:02:33 -0800

    Time: 207 ms (52.36%), Space: 43.3 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -5,9 +5,11 @@ class Solution:
         """
         l=0
         r=len(s)-1
-        while l<r:
-            s[l],s[r]=s[r],s[l]
-            r-=1
-            l+=1
         
-        
+        def reverse(l , r):
+            if l >= r: return s
+            
+            s[l],s[r] = s[r],s[l]
+            
+            return reverse(l + 1, r - 1)
+        reverse(l , r)
