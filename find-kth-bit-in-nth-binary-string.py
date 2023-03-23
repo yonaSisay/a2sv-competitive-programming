432481408adc879da281b87bac3e55091a48b08c
yonas sisay <86576073+yonaSisay@users.noreply.github.com>
2023-03-23 06:51:00 -0700

    added find-kth-bit-in-nth-binary-string.python

--- find-kth-bit-in-nth-binary-string.py
+++ find-kth-bit-in-nth-binary-string.py
@@ -1,7 +1,24 @@
 class Solution:
-    def findKthBit(self, N, K, R = True):
-        if K == 1: return '0' if R else '1'
-        mid = (1 << (N - 1))
-        if K < mid: return self.findKthBit(N - 1, K, R)
-        if K > mid: return self.findKthBit(N - 1, 2 * mid - K, not R)
-        return '1' if R else '0'
+    def invertReverse(self, i):
+        if i == 1:
+            return "0"
+        
+        
+        temp = self.invertReverse(i - 1)
+        
+        temp2 = ""
+
+        for i in range(len(temp)):
+            if temp[i] == "0": 
+                temp2 += "1"
+            else:
+                temp2 += "0"
+
+        
+        temp += "1" + temp2[::-1]
+
+        return temp
+        
+
+    def findKthBit(self, n: int, k: int) -> str:    
+        return self.invertReverse(n)[k - 1]
